b36af17d0c95697804b52556fc728dbdc93a6922
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2022-01-03 17:35:43 +0100

    Update main.rs

--- etsy_get_sales/src/main.rs
+++ etsy_get_sales/src/main.rs
@@ -1,14 +1,24 @@
-use std::io::Empty;
-
 use reqwest;
 use scraper::{Html, Selector};
-use umya_spreadsheet::{self, helper::coordinate::column_index_from_string};
+use umya_spreadsheet::{self, Worksheet};
 fn main() {
+    let path_str = format!(
+        "{}\\2.xlsx",
+        std::env::current_dir().unwrap().to_str().unwrap()
+    );
+    let path = std::path::Path::new(&path_str);
+
+    let workbook = umya_spreadsheet::reader::xlsx::read(&path).unwrap();
+    let sheet = workbook.get_sheet_by_name("Sayfa1").unwrap();
     println!(
         "{}",
         get_sales("https://www.etsy.com/shop/DigitalBoutiqueFinds")
     );
-    get_links();
+    for i in 0..get_links_with_names(sheet)[0].len() {
+        println!("{}", get_links_with_names(sheet)[0][i]);
+        println!("{}", get_links_with_names(sheet)[1][i]);
+        break;
+    }
 }
 
 fn to_unsinged(i: usize) -> u32 {
@@ -29,60 +39,50 @@ fn get_sales(link: &str) -> String {
     return sales;
 }
 
-fn get_links() {
-    let links: Vec<&str> = Vec::new();
-    let path_str = format!(
-        "{}\\2.xlsx",
-        std::env::current_dir().unwrap().to_str().unwrap()
-    );
-    let path = std::path::Path::new(&path_str);
-    // println!("{}", path);
-    let workbook = umya_spreadsheet::reader::xlsx::read(&path).unwrap();
-    let sheet = workbook.get_sheet_by_name("Sayfa1").unwrap();
+fn get_links_with_names(sheet: &Worksheet) -> Vec<Vec<String>> {
+    let mut links: Vec<String> = Vec::new();
+    let mut names: Vec<String> = Vec::new();
+    let mut links_with_names: Vec<Vec<String>> = Vec::new();
     let mut started = false;
     let mut i = 0;
-    while true {
-        if sheet
-            .get_cell_value_by_column_and_row(1, to_unsinged(i))
-            .get_value()
-            == ""
-        {
-            started = false;
-        } else if sheet
-            .get_cell_value_by_column_and_row(1, to_unsinged(i))
-            .get_value()
-            != ""
-        {
-            started = true
-        } else if started
+    loop {
+        if !started
             && sheet
                 .get_cell_value_by_column_and_row(1, to_unsinged(i))
                 .get_value()
-                == ""
+                .is_empty()
+        {
+            i += 1;
+            continue;
+        } else if !sheet
+            .get_cell_value_by_column_and_row(1, to_unsinged(i))
+            .get_value()
+            .is_empty()
+        {
+            started = true
+        } else if sheet
+            .get_cell_value_by_column_and_row(1, to_unsinged(i))
+            .get_value()
+            .is_empty()
         {
             break;
         }
 
-        println!(
-            "{}",
+        links.push(
             sheet
                 .get_cell_value_by_column_and_row(1, to_unsinged(i))
                 .get_value()
+                .to_string(),
         );
-        i = i + 1;
+        names.push(
+            sheet
+                .get_cell_value_by_column_and_row(2, to_unsinged(i))
+                .get_value()
+                .to_string(),
+        );
+        links_with_names.push(links.clone());
+        links_with_names.push(names.clone());
+        i += 1;
     }
-    // let sheet_name= workbook.sheet_names()[0].clone();
-
-    // let range = workbook.worksheet_range(&sheet_name).unwrap().expect("Failed to get range");
-
-    // println!("{:?}", workbook);
-
-    // if let Some(result) = iter.next() {
-    //     let (label, value): (String, f64) = result?;
-    //     assert_eq!(label, "celsius");
-    //     assert_eq!(value, 22.2222);
-    //     Ok(())
-    // } else {
-    //     Err(From::from("expected at least one record but got none"))
-    // }
+    return links_with_names;
 }
